0b41bb7df447b5fb9b34eedf7e34978d977a4aba
MatteoLoddo <loddomatteo96@gmail.com>
2022-05-23 14:41:49 +0200

    ADD COMMENT

--- MAIL/js/main-mail.js
+++ MAIL/js/main-mail.js
@@ -10,31 +10,25 @@ let itemTrovato = -1;
 console.log("messaggio");
 
 // CREARE IL CICLO CHE CONTROLLA SE LA MAIL INSERITA COINCIDE CON UNA DELL'  ARRAY CREATO IN PRECEDENZA
+debugger
+
 for ( let i=0; i<listaMailAccesso.length; i++){
 
-debugger
-    
-
-       // CREARE LE CONDIZIONI IN CUI SE LA MAIL INSERITA COINCIDE STAMPO UN ESITO POSITIVO
-
-    //    let itemNellaLista = listaMailAccesso[i]
-    
-
+       // CREARE LE CONDIZIONI IN CUI SE LA MAIL INSERITA COINCIDE IL LA LET itemTrovato PRENDE IL VALORE DELL INDICE DEL CICLO
 
     if(userMail === listaMailAccesso[i]){
         itemTrovato = i;
 
-        // messaggio.innerHTML=(`<h3>${userMail}</h3><h4>la mail e tra gli utenti registrati, login effettutato con successo!</h4>`)
-
+        console.log(listaMailAccesso[i]);
         console.log(itemTrovato);
     }
 }
-
+// SE COINCIDE IL VALORE  DELL ITEM TROVATO SARA MAGGIORE  DI -1 QUINDI SIA AVVERA LA  PRIMA CONDIZIONE
 if(itemTrovato > -1){
 
     messaggio.innerHTML=(`<h3>${userMail}</h3><h4>la mail e tra gli utenti registrati, login effettutato con successo!</h4>`)
 
-
+// SE  IL VALORE RIMANE  -1  SIGNIFICA  CHE LA MAIL NON  COINCIDE CON NESSUN  ELEMENTO DELL ARRAY CREATO  IN PRECEDENZA
 }else{
     messaggio.innerHTML=(`<h1>!ATTENZIONE!</h1><h3 style="color:red">${userMail}</h3><h4>Questa mail non e ancora stata registrata!</h4>`)
 
